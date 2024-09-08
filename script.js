5a4597f9b6984ba1894565f963b104f06718e4e9
Kat Tyushnyakova <avchimchik@gmail.com>
2024-09-09 00:11:03 +0500

    Списки по порядку у книг 2 и 5

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const library = document.querySelectorAll('.books');
 const book = document.querySelectorAll('.book');
 let title3 = document.querySelector('.title3');
 let advertisement = document.querySelector('.adv');
+const book2 = document.querySelectorAll('.listBook2');
+const book5 = document.querySelectorAll('.listBook5');
 
 book[0].before(book[1]);
 book[3].before(book[4]);
@@ -11,4 +13,14 @@ document.body.style.backgroundImage = "url('image/library.jpg')";
 title3.textContent = 'Книга 3. this и Прототипы Объектов';
 advertisement.remove();
 
+book2[4].before(book2[6]);
+book2[4].before(book2[8]);
+book2[9].after(book2[2]);
+
+book5[4].after(book5[2]);
+book5[3].before(book5[9]);
+book5[7].after(book5[5]);
+
 console.log(library, book);
+console.log(book2);
+console.log(book5);
