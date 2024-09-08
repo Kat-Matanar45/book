944a6dd58f7030915ce55eee204d4f2fab9bb6be
Kat Tyushnyakova <avchimchik@gmail.com>
2024-09-09 00:28:14 +0500

    Все работает!

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let title3 = document.querySelector('.title3');
 let advertisement = document.querySelector('.adv');
 const book2 = document.querySelectorAll('.listBook2');
 const book5 = document.querySelectorAll('.listBook5');
+const book6 = document.querySelectorAll('.listBook6');
+const chapter = document.createElement('li');
 
 book[0].before(book[1]);
 book[3].before(book[4]);
@@ -21,6 +23,11 @@ book5[4].after(book5[2]);
 book5[3].before(book5[9]);
 book5[7].after(book5[5]);
 
+chapter.textContent = 'Глава 8: За пределами ES6';
+chapter.classList.add('listBook6');
+book6[8].append(chapter);
+
 console.log(library, book);
 console.log(book2);
 console.log(book5);
+console.log(book6);
