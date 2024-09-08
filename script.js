3a8874244aaf9838fd641fec6c7c67ceb3c1a4ba
Kat Tyushnyakova <avchimchik@gmail.com>
2024-09-08 21:35:31 +0500

    Код отредактирован

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const library = document.querySelectorAll('.books');
 const book = document.querySelectorAll('.book');
-
-console.log(library, book);
+let title3 = document.querySelector('.title3');
+let advertisement = document.querySelector('.adv');
 
 book[0].before(book[1]);
 book[3].before(book[4]);
 book[5].after(book[2]);
 
 document.body.style.backgroundImage = "url('image/library.jpg')";
-
-let title3 = document.querySelector('.title3');
 title3.textContent = 'Книга 3. this и Прототипы Объектов';
-
-let advertisement = document.querySelector('.adv');
 advertisement.remove();
+
+console.log(library, book);
