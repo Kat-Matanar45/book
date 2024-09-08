2e59df9dcd446edcfb3ea06e0f9360d251fdcaa4
Kat Tyushnyakova <avchimchik@gmail.com>
2024-09-08 21:29:36 +0500

    Исправлен заголовок

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ book[3].before(book[4]);
 book[5].after(book[2]);
 
 document.body.style.backgroundImage = "url('image/library.jpg')";
+
+let title3 = document.querySelector('.title3');
+title3.textContent = 'Книга 3. this и Прототипы Объектов';
